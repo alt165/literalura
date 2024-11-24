cb98689a5fc7aed4660a704112c5046cf1366e88
Jose Lambrechts <alt165@gmail.com>
2024-11-24 19:28:12 -0300

    Primer Commit, crear proyecto spring

---
+++ src/test/java/com/alt165/Literalura/LiteraluraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alt165.Literalura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteraluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
